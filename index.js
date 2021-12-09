2f78b0289eb4b36900c10c9a03e02756c43ac361
Md Ahmed <maahmed126@gmail.com>
2021-12-09 23:45:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
+const { v4: uuidv4 } = require('uuid');
 const { sqrt } = require('mathjs')
+
 exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
         body: JSON.stringify('Hello from Lambda!'),
     };
-    console.log(sqrt(-4).toString()) // 2i
-    return response;
     
+    console.log(sqrt(-4).toString()); // 2i
+    return uuidv4();
 
 };
